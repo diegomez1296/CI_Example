94562908a6e4bff9617b322ad69a76fdb35f7f82
Lukasz <rlukasz1996@gmail.com>
2019-05-14 12:33:31 +0200

    Initial commit

---
+++ src/main/java/rydzinski/ciexample/Calculator.java
@@ -0,0 +1,8 @@
+package rydzinski.ciexample;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/main/java/rydzinski/ciexample/CiExampleApplication.java
@@ -0,0 +1,13 @@
+package rydzinski.ciexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiExampleApplication.class, args);
+	}
+
+}
